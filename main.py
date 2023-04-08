cadc9e0ee98cf7012dd96d65e2aa74eda7eb76d1
Yuki2Kisaragi <masahero.yuffie@outlook.jp>
2023-04-09 01:13:22 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+def print_hi(name):
+    # スクリプトをデバッグするには以下のコード行でブレークポイントを使用してください。
+    print(f'Hi, {name}')  # Ctrl+F8を押すとブレークポイントを切り替えます。
+
+
+# ガター内の緑色のボタンを押すとスクリプトを実行します。
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# PyCharm のヘルプは https://www.jetbrains.com/help/pycharm/ を参照してください
