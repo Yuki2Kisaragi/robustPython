1a02332f3591e41efbe7ae57d43e78f020a25801
Yuki2Kisaragi <masahero.yuffie@outlook.jp>
2023-04-10 00:28:36 +0900

    changed Ruff rule
    - GitHubActions追加
    - RuffRule変更

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ import os
 import sys
 
 
-def greeting_path(msg, name):
+def greeting_path(msg: str, name: str) -> None:
     print(sys.argv)
     os.get_exec_path()
     print(f"{msg}, {name}")
 
 
-def print_hi(name) -> None:
+def convert_uppercase(msg: str) -> str:
+    return msg.upper()
+
+
+def print_hi(name: str) -> None:
     # スクリプトをデバッグするには以下のコード行でブレークポイントを使用してください。
-    print(f'Hi, {name}')  # Ctrl+F8を押すとブレークポイントを切り替えます。
+    print(f"Hi, {convert_uppercase(name)}")  # Ctrl+F8を押すとブレークポイントを切り替えます。
 
 
 # ガター内の緑色のボタンを押すとスクリプトを実行します。
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if __name__ == "__main__":
+    print_hi("pycharm")
 
 # PyCharm のヘルプは https://www.jetbrains.com/help/pycharm/ を参照してください
